dbc94ae1d536c7147bed3354e20c52ea0937e5f1
Rahul Sanjay Gosavi <rahulgosavi624@gmail.com>
2025-03-27 19:59:39 +0530

    Update fizzbuzz.py

--- Learnings/Questions/fizzbuzz.py
+++ Learnings/Questions/fizzbuzz.py
@@ -2,12 +2,17 @@
 # For multiples of 3, print "Fizz", for multiples of 5 print "Buzz", 
 # and for multiples of both print "FizzBuzz".
 
-for i in range(1, 101):  # Loop from 1 to 100
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+def fizzbuzz(n):
+    for i in range(1, n + 1):  # Loop from 1 to n
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Take input from user
+n = int(input("Enter the range for FizzBuzz: "))
+fizzbuzz(n)
