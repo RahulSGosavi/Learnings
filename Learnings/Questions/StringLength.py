6a261e123ab8343c5ef5b815968de29c71fd1838
Rahul Sanjay Gosavi <rahulgosavi624@gmail.com>
2025-03-27 18:54:17 +0530

    Add files via upload

---
+++ Learnings/Questions/StringLength.py
@@ -0,0 +1,8 @@
+# Question 1: Write a function that takes a string and returns its length.
+
+def string_length(s):
+    return len(s)
+
+# Example usage
+string = "Hello, World!"
+print(string_length(string)) 
